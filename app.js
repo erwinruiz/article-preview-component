211828229b86111d1d7da9d41833f1358cc5c8ce
Erwin Ruiz <88996407+erwinruiz@users.noreply.github.com>
2021-08-23 14:59:37 -0700

    Change functionality on mobile design (share option) to a modal

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const shareIcon = document.querySelector(".share-icon");
+const modalShareIcon = document.querySelector(".modal-share-icon");
 const content = document.querySelector(".content");
 
 shareIcon.addEventListener("click", () => {
   if (!document.querySelector(".active")) {
-    document.querySelector(".details").classList.add("active");
-  } else {
-    document.querySelector(".details").classList.remove("active");
+    document.querySelector(".modal").classList.add("active");
   }
 });
 
+modalShareIcon.addEventListener("click", () => {
+  document.querySelector(".modal").classList.remove("active");
+});
+
 content.addEventListener("click", () => {
-  document.querySelector(".details").classList.remove("active");
+  document.querySelector(".modal").classList.remove("active");
 });
