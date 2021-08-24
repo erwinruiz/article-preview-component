77aef3610c7f91b4e2de9c8c89197734628fdac6
Erwin Ruiz <88996407+erwinruiz@users.noreply.github.com>
2021-08-23 19:13:45 -0700

    Add functionality to desktop design (share option)

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const content = document.querySelector(".content");
 shareIcon.addEventListener("click", () => {
   if (!document.querySelector(".active")) {
     document.querySelector(".modal").classList.add("active");
+    document.querySelector(".modal-arrow").classList.add("active-arrow");
+  } else {
+    document.querySelector(".modal").classList.remove("active");
+    document.querySelector(".modal-arrow").classList.remove("active-arrow");
   }
 });
 
 modalShareIcon.addEventListener("click", () => {
   document.querySelector(".modal").classList.remove("active");
+  document.querySelector(".modal-arrow").classList.remove("active-arrow");
 });
 
 content.addEventListener("click", () => {
   document.querySelector(".modal").classList.remove("active");
+  document.querySelector(".modal-arrow").classList.remove("active-arrow");
 });
